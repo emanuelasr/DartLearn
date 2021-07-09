3b86df8d9e3cfad2c174cfbf0c43e4cf8e63265d
Emanuel Ramos <emanuelasrm@hotmail.com>
2021-07-09 15:22:53 -0300

    criação da classe 'enemy' do rpg

--- EnemyClass.dart
+++ EnemyClass.dart
@@ -2,6 +2,7 @@ class Orc {
   String name = 'manel';
   String height = '175 CM';
   String sPower = 'Rajada do Leão';
+
   var pwr = 120;
   var def = 200;
   var smrt = 50;
@@ -11,6 +12,7 @@ class Oars {
   String Nome = '';
   String Height = "";
   String SPower = "";
+
   var pwr = 100;
   var def = 200;
   var smrt = 5;
