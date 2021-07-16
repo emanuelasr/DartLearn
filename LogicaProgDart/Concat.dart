857f6da4f88d3ec68296d985aab5864aacee774c
Emanuel Ramos <emanuelasrm@hotmail.com>
2021-07-16 10:56:04 -0300

    Criação de duas novas pastas

---
+++ LogicaProgDart/Concat.dart
@@ -0,0 +1,9 @@
+// nesse tutorial , vou fazer uma concatenação de dados. seja strings ou soma de
+// valores com as variaveis
+
+main() {
+  var Emanuel = 'é gay ';
+  var nome = 'Douglas ';
+
+  print(nome + Emanuel);
+}
