0caad6f3ae55af8aae7be28ab17834bd2f8242d8
emanuelasr <emanuelasrm@hotmail.com>
2021-07-29 09:11:08 -0300

    Esse é um Print, as formas que eu posso mostrar algo pro usuario.

---
+++ LogicaProgDart/Print.dart
@@ -0,0 +1,19 @@
+/* types of 'Output' 
+ in dart we have some ways to show a message to user;
+ we can show something by using a 'print' method, and putting some data 
+inside of it as the example : print('here we put some information');
+
+but we can show a input from user inside of this 'print' too .
+if we declare a variable called 'name' and take the data from user, this variable
+will contain now a name or something that he wrote/type to us. to show that, we have to called this 
+variable inside my print statement. for exmpl; 
+
+var name = manel; 
+print(name) -> here I have to call my variable. this output it'll be 'manel' which is the data stored inside of the variable called name. 
+
+we have another thing that we can do too , which is mixed a little bit of text and my variable. if I want to show a message to the user 
+and her name I can do that. is just I put a ' quotation mark inside of the print and then calling my variable with a dollar sign '$'
+than i can show both at the same time , my question and the data inside my variable; for exmp:
+
+  PRINT('something that i want to user see $name <- and the name of the user'); 
+*/ 
